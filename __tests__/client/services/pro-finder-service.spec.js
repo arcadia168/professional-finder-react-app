e9c2c81c3b632aa51b17ad269047290754cfb726
Erin Eland <erin@erineland.com>
2019-06-18 21:29:13 +0100

    TDD - Passing in an axios instance into the proFinderService.

--- __tests__/client/services/pro-finder-service.spec.js
+++ __tests__/client/services/pro-finder-service.spec.js
@@ -1,14 +1,28 @@
 import ProFinderService from '../../../src/client/service/pro-finder-service';
 
 describe('Pro Finder Api Service', () => {
+    let mockAxiosPostSearch = jest.fn(() => Promise.resolve());
+    let mockAxios = {
+        get: mockAxiosPostSearch,
+    };
+
     describe('When a pro finder api service is instantiated', () => {
+        let proFinderServiceInstance;
+
+        beforeEach(() => {
+            proFinderServiceInstance = new ProFinderService(mockAxios);
+        });
+
         it('Returns an instance of the service', () => {
-            const proFinderServiceInstance = new ProFinderService();
             expect(
                 proFinderServiceInstance
             ).toBeInstanceOf(
                 ProFinderService
             )
+        });
+
+        it('Can take an instance of axios and assign this to a property', () => {
+            expect(proFinderServiceInstance.axios).toEqual(mockAxios);
         });
     })
 });
