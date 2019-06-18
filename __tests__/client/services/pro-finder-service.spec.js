25451b818da9289c68c066ef74b6a9eb2ec49590
arcadia168 <bennawaz168@icloud.com>
2019-06-18 22:46:11 +0100

    TDD - Retrieving the list of professions from the JSON file and returning them.

--- __tests__/client/services/pro-finder-service.spec.js
+++ __tests__/client/services/pro-finder-service.spec.js
@@ -1,4 +1,9 @@
 import ProFinderService from '../../../src/client/service/pro-finder-service';
+import path from 'path';
+import fs from 'fs';
+
+const pathName = path.resolve(__dirname, `../../../src/client/service/professions.json`);
+const mockProfessions = JSON.parse(fs.readFileSync(pathName, 'utf8'));
 
 describe('Pro Finder Api Service', () => {
     let mockAxiosPostSearch = jest.fn(() => Promise.resolve());
@@ -13,6 +18,10 @@ describe('Pro Finder Api Service', () => {
             proFinderServiceInstance = new ProFinderService(mockAxios);
         });
 
+        afterEach(() => {
+            jest.clearAllMocks();
+        });
+
         it('Returns an instance of the service', () => {
             expect(
                 proFinderServiceInstance
@@ -23,6 +32,15 @@ describe('Pro Finder Api Service', () => {
 
         it('Can take an instance of axios and assign this to a property', () => {
             expect(proFinderServiceInstance.axios).toEqual(mockAxios);
+        });
+
+        it('Returns a list of professions', () => {
+            const professionList = proFinderServiceInstance.getProfessions();
+            expect(
+                professionList
+            ).toEqual(
+                mockProfessions
+            )
         });
     })
 });
