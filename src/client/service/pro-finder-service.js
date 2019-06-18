2d9db6ce7dfcb90b61350729222b09826586eb42
arcadia168 <bennawaz168@icloud.com>
2019-06-18 22:58:20 +0100

    A refactor of the previous code (no new functionality added) - using 'profession categories' domain term now.

--- src/client/service/pro-finder-service.js
+++ src/client/service/pro-finder-service.js
@@ -1,14 +1,13 @@
 import path from 'path';
 import fs from 'fs';
 
-const pathName = path.resolve(__dirname, `../../../src/client/service/professions.json`);
-
 export default class ProFinderService {
     constructor(axiosInstance) {
         this.axios = axiosInstance;
     }
 
-    getProfessions() {
+    getProfessionCategories() {
+        const pathName = path.resolve(__dirname, `../../../src/client/service/profession-categories.json`);
         const professions = JSON.parse(fs.readFileSync(pathName, 'utf8'));
         const visibleProfessions = professions.filter(currentProfession => !currentProfession.hidden);
         return visibleProfessions;
