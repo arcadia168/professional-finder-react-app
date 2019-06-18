dbf0853652445ad3d09e164370309e193975fe52
Erin Eland <erin@erineland.com>
2019-06-18 22:46:11 +0100

    TDD - Retrieving the list of professions from the JSON file and returning them.

--- src/client/service/pro-finder-service.js
+++ src/client/service/pro-finder-service.js
@@ -1,5 +1,15 @@
+import path from 'path';
+import fs from 'fs';
+
+const pathName = path.resolve(__dirname, `../../../src/client/service/professions.json`);
+
 export default class ProFinderService {
     constructor(axiosInstance) {
         this.axios = axiosInstance;
+    }
+
+    getProfessions() {
+        const professions = JSON.parse(fs.readFileSync(pathName, 'utf8'));
+        return professions;
     }
 }
