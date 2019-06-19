6f2c089cf600921d934036374801651e891232ca
Erin Eland <erin@erineland.com>
2019-06-19 23:20:31 +0100

    TDD - Creating the first react component and testing with Enzyme.

---
+++ __tests__/client/components/professional-finder.spec.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ProfessionalFinder from '../../../src/client/components/ProfessionalFinder';
+import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
+import path from 'path';
+import fs from 'fs';
+
+// const pathName = path.resolve(__dirname, `../../__mocks__/validCardResults.json`);
+// const mockCards = fs.readFileSync(pathName, 'utf8');
+
+describe('Professional Finder', () => {
+    const render = customProps => {
+        const props = {
+            // Default props
+            ...customProps,
+        }
+        return mount(<ProfessionalFinder {...props}/>);
+    }
+
+    it('renders the app as expected', () => {
+        const component = renderer.create(
+            <ProfessionalFinder />,
+        );
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('successfully renders the container for the layout', () => {
+        const renderedApp = render();
+        expect(
+            renderedApp
+                .find('[data-testid="pro-finder__container"]')
+                .exists()
+        ).toBeTruthy();
+    });
+});
