0a616cf7a1be7a6627cb61a8a7b3b2563f6a87b7
Erin Eland <erin@erineland.com>
2019-06-20 23:17:23 +0100

    WIP - Moving read of JSON to server and testing using supertest.

--- src/client/index.js
+++ src/client/index.js
@@ -4,7 +4,6 @@ import App from './components/App.jsx';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import styles from './scss/application.scss';
 import axios from 'axios';
-import proFinderService from '../client/service/pro-finder-service';
 import ProFinderService from '../client/service/pro-finder-service';
 
 const axiosInstance = axios;
