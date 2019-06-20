2f3c689a4a819a21ad87329278c0ca82fc78e1c9
arcadia168 <bennawaz168@icloud.com>
2019-06-20 21:47:58 +0100

    Adding new test to app to ensure prop is set.

--- src/client/components/contexts/local-pros-context.js
+++
@@ -1,4 +0,0 @@
-export const LocalProsContext = React.createContext({
-    localPros: [],
-    searchLocalPros: () => {}
-});
--- src/client/index.js
+++ src/client/index.js
@@ -4,8 +4,11 @@ import App from './components/App.jsx';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import styles from './scss/application.scss';
 import axios from 'axios';
+import proFinderService from '../client/service/pro-finder-service';
+import ProFinderService from '../client/service/pro-finder-service';
 
 const axiosInstance = axios;
+const proFinderService = new ProFinderService(axiosInstance);
 
 render(
   <App />,
