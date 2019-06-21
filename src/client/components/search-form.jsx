966c5e0ae7e3d2cc0a17090bed5e7b2956fe7a2d
Erin Eland <erin@erineland.com>
2019-06-21 13:25:09 +0100

    TDD - Search button text and removing not neede container elements.

--- src/client/components/search-form.jsx
+++ src/client/components/search-form.jsx
@@ -10,21 +10,15 @@ class SearchForm extends Component {
     render() {
         return (
             <div data-testid="search-form__container">
-                <div
-                    data-testid="search-form__category-dropdown-container"
-                    className="search-form__category-dropdown-container"
+                <Dropdown
+                    data-testid="search-form__category-dropdown"
+                    className="search-form__category-dropdown"
                 >
-                    <Dropdown
-                        data-testid="search-form__category-dropdown"
-                        className="search-form__category-dropdown"
-                    >
-                    <Button data-testid="search-form__search-btn" className="search-form__search-btn">
-
-                    </Button>
-                    </Dropdown>
-
-                </div>
-            </div>
+                </Dropdown>
+                <Button data-testid="search-form__search-btn" className="search-form__search-btn">
+                    Search
+                </Button>
+            </div >
         );
     };
 }
