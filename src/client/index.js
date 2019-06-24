c09a8a98d4a8e573fd59e7c0180b945678b91efb
arcadia168 <bennawaz168@icloud.com>
2019-06-24 22:40:55 +0100

    Successfully rendering the search form using react-redux connect and Provider.

--- src/client/index.js
+++ src/client/index.js
@@ -3,14 +3,17 @@ import { render } from 'react-dom';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import styles from './scss/application.scss';
 import { combineReducers, applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
 import promise from 'redux-promise-middleware'
-import App from './components/app.jsx';
 // import ProfessionalFinder from './components/professional-finder.jsx'
-import ProFinderService from '../client/service/pro-finder-service';
 import proCategory from './reducers/proCategory.js';
 import proLocation from './reducers/proLocation.js';
 import proCategories from './reducers/proCategories.js';
 import searchResults from './reducers/searchResults.js';
+// import App from './components/app.jsx';
+import ProFinderService from '../client/service/pro-finder-service';
+import SearchForm from '../client/components/search-form.jsx';
+
 
 const proFinderApp = combineReducers({
   proCategory,
@@ -21,20 +24,15 @@ const proFinderApp = combineReducers({
 
 const proFinderStore = createStore(proFinderApp, applyMiddleware(promise));
 
-const renderProFinder = () => {
-  render(
-    <App
-      localProValues={proFinderStore.getState()}
-      store={proFinderStore}
-    />,
-    document.getElementById('root')
-  );
-}
-
 proFinderStore.dispatch({
   type: 'PRO_CATEGORIES',
   payload: ProFinderService.getProfessionCategories()
 });
 
-proFinderStore.subscribe(renderProFinder);
-renderProFinder();
+render(
+  <Provider store={proFinderStore}>
+    <SearchForm />
+  </Provider>,
+    document.getElementById('root')
+);
+
