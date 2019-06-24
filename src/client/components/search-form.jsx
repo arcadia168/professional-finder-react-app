e7f0b7382ec9a144ef5de3a94fbaea6cc03344e3
arcadia168 <bennawaz168@icloud.com>
2019-06-24 23:11:57 +0100

    Rewriting search-form to be a pure function component.

--- src/client/components/search-form.jsx
+++ src/client/components/search-form.jsx
@@ -4,99 +4,92 @@ import PropTypes from 'prop-types'
 import ProFinderService from '../service/pro-finder-service';
 import { connect } from 'react-redux';
 
-class SearchForm extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    componentDidMount() {
-        this.updateInputValue = evt => {
-            this.props.updateLocation(evt.target.value);
-        }
-
-        this.handleCategoryChosen = (evtKey, evt) => {
-            const categoryParams = {
-                categoryName: evt.currentTarget.text,
-                categoryId: evtKey
-            };
-            this.props.updateCategory(categoryParams);
-        }
-
-        this.handleSearchBtn = () => {
-            let locationValidation;
-            const postcode = this.props.location;
-
-            if (postcode) {
-                const squishedPostcode = postcode.replace(/\s/g, "");
-                const regex = /^[A-Z]{1,2}[0-9]{1,2}[A-Z]{0,1}/i
-                locationValidation = regex.test(squishedPostcode);
-            }
-
-            if (!this.props.chosenCategoryId) {
-                this.props.setError('Please choose a category of job.');
-            } else if (!locationValidation) {
-                this.props.setError('Please enter a valid UK postcode...');
-            } else {
-                const searchParams = {
-                    location: this.props.location,
-                    categoryId: this.props.chosenCategoryId
-                };
-                this.props.searchLocalPros(searchParams)
-            }
-        }
-    }
-
-    render() {
-        debugger;
-        return (
-            <div data-testid="search-form__container" className="search-form__container">
-                <DropdownButton
-                    title={this.props.chosenCategoryName || 'Choose a category'}
-                    onSelect={this.handleCategoryChosen}
-                    data-testid="search-form__category-dropdown"
-                    className="search-form__category-dropdown"
+const SearchForm = ({
+    chosenCategoryName,
+    chosenCategoryId,
+    categoriesLoading,
+    categories,
+    location,
+    updateCategory,
+    updateLocation,
+    searchLocalPros
+}) => {
+    return (
+        <div data-testid="search-form__container" className="search-form__container">
+            <DropdownButton
+                title={chosenCategoryName || 'Choose a category'}
+                onSelect={(evtKey, evt) => {
+                    const categoryParams = {
+                        categoryName: evt.currentTarget.text,
+                        categoryId: evtKey
+                    };
+                    updateCategory(categoryParams);
+                }}
+                data-testid="search-form__category-dropdown"
+                className="search-form__category-dropdown"
+            >
+                {
+                    categoriesLoading ?
+                        <Spinner animation="border" role="status">
+                            <span className="sr-only">Loading Categories</span>
+                        </Spinner>
+                        :
+                        categories ? categories.map(category => {
+                            return <Dropdown.Item
+                                key={category.id}
+                                eventKey={category.id}
+                            >
+                                {category.name}
+                            </Dropdown.Item>
+                        }) : null
+                }
+            </DropdownButton>
+            <InputGroup
+                data-testid="search-form__search-field"
+                className="search-form__search-field"
+            >
+                <FormControl
+                    data-testid="search-form__search-field-input"
+                    className="search-form__search-field-input"
+                    placeholder="Enter UK Postcode here..."
+                    aria-label="Enter UK Postcode here..."
+                    onChange={evt => {
+                        updateLocation(evt.target.value);
+                    }}
                 >
-                    {
-                        this.props.categoriesLoading ?
-                            <Spinner animation="border" role="status">
-                                <span className="sr-only">Loading Categories</span>
-                            </Spinner>
-                            :
-                            this.props.categories ? this.props.categories.map(category => {
-                                return <Dropdown.Item
-                                    key={category.id}
-                                    eventKey={category.id}
-                                >
-                                    {category.name}
-                                </Dropdown.Item>
-                            }) : null
+                </FormControl>
+            </InputGroup>
+            <Button
+                data-testid="search-form__search-btn"
+                className="search-form__search-btn"
+                onClick={() => {
+                    let locationValidation;
+                    const postcode = location;
+
+                    if (postcode) {
+                        const squishedPostcode = postcode.replace(/\s/g, "");
+                        const regex = /^[A-Z]{1,2}[0-9]{1,2}[A-Z]{0,1}/i
+                        locationValidation = regex.test(squishedPostcode);
                     }
-                </DropdownButton>
-                <InputGroup
-                    data-testid="search-form__search-field"
-                    className="search-form__search-field"
-                >
-                    <FormControl
-                        data-testid="search-form__search-field-input"
-                        className="search-form__search-field-input"
-                        placeholder="Enter UK Postcode here..."
-                        aria-label="Enter UK Postcode here..."
-                        ref="searchField"
-                        onChange={this.updateInputValue}
-                    >
-                    </FormControl>
-                </InputGroup>
-                <Button
-                    data-testid="search-form__search-btn"
-                    className="search-form__search-btn"
-                    onClick={this.handleSearchBtn}
-                >
-                    Submit
+
+                    if (!chosenCategoryId) {
+                        setError('Please choose a category of job.');
+                    } else if (!locationValidation) {
+                        setError('Please enter a valid UK postcode...');
+                    } else {
+                        const searchParams = {
+                            location: location,
+                            categoryId: chosenCategoryId
+                        };
+                        searchLocalPros(searchParams)
+                    }
+                }}
+            >
+                Submit
                 </Button>
-            </div >
-        );
-    };
-}
+        </div >
+    );
+};
 
 const mapStateToProps = state => {
     debugger;
